f00222fe1dca184b0e60209271e63b964d8df919
hernans <hsilgueira@gmail.com>
2025-03-31 08:15:54 -0300

    Dockerfile y demas archivos que use para probar todo

--- source/vue-project/src/components/HelloWorld.vue
+++ source/vue-project/src/components/HelloWorld.vue
@@ -6,36 +6,65 @@ defineProps({
 })
 
 const count = ref(0)
+
+  import { ref } from 'vue'
+
+  const location = ref('end')
+
+  const items = [
+    { title: 'Click Me' },
+    { title: 'Click Me' },
+    { title: 'Click Me' },
+    { title: 'Click Me 2' },
+  ]
+  const locations = [
+    'top',
+    'bottom',
+    'start',
+    'end',
+    'center',
+  ]
+
+
 </script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="text-center">
+    <v-select
+      v-model="location"
+      :items="locations"
+      label="Location"
+    ></v-select>
+    <v-menu :location="location">
+      <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          Dropdown
+        </v-btn>
+      </template>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+<template>
 
+  <v-card
+    class="mx-auto"
+    max-width="300"
+  >
+    <v-list :items="items"></v-list>
+  </v-card>
+</template>
 <style scoped>
 .read-the-docs {
   color: #888;
