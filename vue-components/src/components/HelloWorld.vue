cb3258e249459b833c5a42a143797b4cbf9269ef
hernans <hsilgueira@gmail.com>
2025-03-31 14:08:25 -0300

    agregando componentes proyecto vuejs

---
+++ vue-components/src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+
+  const items = [
+    {
+      title: 'Item #1',
+      value: 1,
+    },
+    {
+      title: 'Item #2',
+      value: 2,
+    },
+    {
+      title: 'Item #3',
+      value: 3,
+    },
+  ]
+</script>
+
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="300"
+  >
+    <v-list :items="items"></v-list>
+  </v-card>
+
+  <v-tooltip text="Tooltip">
+  <template v-slot:activator="{ props }">
+    <v-btn v-bind="props">Hover Over Me</v-btn>
+  </template>
+</v-tooltip><br>
+<v-dialog max-width="500">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      v-bind="activatorProps"
+      color="surface-variant"
+      text="Open Dialog"
+      variant="flat"
+    ></v-btn>
+  </template>
+
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+</template>
+
+
+
